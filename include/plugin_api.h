054c5f7e58b3b082f117a62c62c838e608df3bf5
Tuomas Virtanen <katajakasa@gmail.com>
2014-04-05 18:30:20 +0300

    Make sure stuff compiles on linux

--- include/plugin_api.h
+++ include/plugin_api.h
@@ -1,7 +1,11 @@
 #ifndef _PLUGIN_API
 #define _PLUGIN_API
 
+#ifdef _WIN32
 #define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
 
 extern "C" {
 const char* EXPORT plugin_get_name();
